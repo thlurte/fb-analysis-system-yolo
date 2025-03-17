22f830d2286b5769222c6694dcbff8bd4e67fb8e
thlurte <s92062851@ousl.lk>
2025-03-17 16:03:44 +0530

    add: track objects

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from utils.video_processor.processor.video_processor import VideoProcessor
 from utils.video_processor.reader.video_reader import VideoReader
 from utils.video_processor.writer.video_writer import VideoWriter
-
+from trackers import Tracker
 def main():
     # read video frames
     reader = VideoReader('input_videos/08fd33_4.mp4')
     writer = VideoWriter('output_videos/08fd33_4.avi')
 
-    # inference
-
     # tracking
+    tracker = Tracker('models/best.pt')
+
+    tracks = tracker.get_object_tracks(reader.read(),
+                                       read_from_stub=True,
+                                       stub_path='stubs/track_stubs.pkl')
 
     # write frames into a video
     video_processor = VideoProcessor(reader, writer)
